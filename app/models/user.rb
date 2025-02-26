b752eee30abab8210df3b6506a609b9e22270249
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-02-26 05:19:49 +0000

    favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,11 @@ class User < ApplicationRecord
   
   has_many :books, dependent: :destroy 
   has_one_attached :profile_image
+  has_many :favorites, dependent: :destroy
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  validates :user_id, uniqueness: {scope: :book_id}
 
   
   
