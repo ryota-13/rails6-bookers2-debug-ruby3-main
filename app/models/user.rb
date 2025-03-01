ff14a1f43b964825b7450659576b091a912eeacc
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-03-01 08:00:45 +0000

    search

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,18 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
   
   
   def get_profile_image(width, height)
